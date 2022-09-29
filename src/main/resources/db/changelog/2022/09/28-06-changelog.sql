5728fbfc9711079eadf1ab686a9c88bf067148ba
Rayan Tawfik Altamimi <raaltamimi@tahakom.com>
2022-09-29 09:59:10 +0300

    - Added rollback scripts.

--- src/main/resources/db/changelog/2022/09/28-06-changelog.sql
+++ src/main/resources/db/changelog/2022/09/28-06-changelog.sql
@@ -8,9 +8,12 @@
                                                    | |
                                                    |_|
      */
-
+-- changeset raaltamimi:1
 insert into author (first_name, last_name)
 values (N'Some', N'Author')
+-- rollback delete top 1 from author where first_name = N'Some' and last_name = N'Author'
 
+-- changeset raaltamimi:2
 insert into book_author (book_id, author_id)
 values ((select top 1 id from book), (select top 1 id from author))
+-- rollback delete top 1 from book_author where book_id = (select top 1 id from book) and  author_id = (select top 1 id from author)
